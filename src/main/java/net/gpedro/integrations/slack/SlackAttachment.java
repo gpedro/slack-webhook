b10276a0551cc21ebabd4e49f40e2d1a5e09ab9d
David Webb <dwebb@prowaveconsulting.com>
2016-06-06 14:30:41 -0400

    Enable support for Markdown in SlackAttachment (#18)
    resolves #4
    
    * Add Markdown Support.
    Formatting file based on original author's formatting policy to make deltas easier to read.

--- src/main/java/net/gpedro/integrations/slack/SlackAttachment.java
+++ src/main/java/net/gpedro/integrations/slack/SlackAttachment.java
@@ -1,11 +1,21 @@
 package net.gpedro.integrations.slack;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Represents an attachment in a slack webhook JSON message.
+ * 
+ * @author Gabriel Pedro
+ * @author David Webb
+ *
+ */
 public class SlackAttachment {
 
     private static final String HEX_REGEX = "^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
@@ -21,6 +31,7 @@ public class SlackAttachment {
     private static final String TITLE_LINK = "title_link";
     private static final String IMAGE_URL = "image_url";
     private static final String THUMB_URL = "thumb_url";
+    private static final String MRKDWN_IN = "mrkdwn_in";
 
     private String fallback;
     private String text;
@@ -33,229 +44,259 @@ public class SlackAttachment {
     private String titleLink;
     private String imageUrl;
     private String thumbUrl;
+    private Set<String> markdownAttributes = new HashSet<String>();
     private List<SlackField> fields = new ArrayList<SlackField>();
 
     public SlackAttachment addFields(SlackField field) {
-        this.fields.add(field);
+	this.fields.add(field);
 
-        return this;
+	return this;
+    }
+
+    public SlackAttachment addMarkdownAttribute(String attr) {
+	this.markdownAttributes.add(attr);
+
+	return this;
     }
 
     private boolean isHex(String pair) {
-        return pair.matches(HEX_REGEX);
+	return pair.matches(HEX_REGEX);
     }
 
     private JsonArray prepareFields() {
-        final JsonArray data = new JsonArray();
-        for (SlackField field : fields) {
-            data.add(field.toJson());
-        }
+	final JsonArray data = new JsonArray();
+	for (SlackField field : fields) {
+	    data.add(field.toJson());
+	}
 
-        return data;
+	return data;
     }
 
     public SlackAttachment removeFields(int index) {
-        this.fields.remove(index);
+	this.fields.remove(index);
 
-        return this;
+	return this;
+    }
+
+    private JsonArray prepareMarkdownAttributes() {
+	final JsonArray data = new JsonArray();
+	for (String attr : markdownAttributes) {
+	    data.add(new JsonPrimitive(attr));
+	}
+
+	return data;
+    }
+
+    public SlackAttachment removeMarkdownAttribute(String attr) {
+	this.markdownAttributes.remove(attr);
+
+	return this;
     }
 
     public SlackAttachment setColor(String color) {
-        if (color != null) {
-            if (color.charAt(0) == '#') {
-                if (!isHex(color.substring(1))) {
-                    throw new IllegalArgumentException(
-                            "Invalid Hex Color @ SlackAttachment");
-                }
-            } else
-                if (!color.matches("^(good|warning|danger)$")) {
-                    throw new IllegalArgumentException(
-                            "Invalid PreDefined Color @ SlackAttachment");
-                }
-        }
+	if (color != null) {
+	    if (color.charAt(0) == '#') {
+		if (!isHex(color.substring(1))) {
+		    throw new IllegalArgumentException("Invalid Hex Color @ SlackAttachment");
+		}
+	    } else if (!color.matches("^(good|warning|danger)$")) {
+		throw new IllegalArgumentException("Invalid PreDefined Color @ SlackAttachment");
+	    }
+	}
 
-        this.color = color;
+	this.color = color;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setFallback(String fallback) {
-        this.fallback = fallback;
+	this.fallback = fallback;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setFields(List<SlackField> fields) {
-        this.fields = fields;
+	this.fields = fields;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setPretext(String pretext) {
-        this.pretext = pretext;
+	this.pretext = pretext;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setText(String text) {
-        this.text = text;
+	this.text = text;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setAuthorName(String authorName) {
-        this.authorName = authorName;
+	this.authorName = authorName;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setAuthorLink(String authorLink) {
-        this.authorLink = authorLink;
+	this.authorLink = authorLink;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setAuthorIcon(String authorIcon) {
-        this.authorIcon = authorIcon;
+	this.authorIcon = authorIcon;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setTitle(String title) {
-        this.title = title;
+	this.title = title;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setTitleLink(String titleLink) {
-        this.titleLink = titleLink;
+	this.titleLink = titleLink;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+	this.imageUrl = imageUrl;
 
-        return this;
+	return this;
     }
 
     public SlackAttachment setThumbUrl(String thumbUrl) {
-        this.thumbUrl = thumbUrl;
+	this.thumbUrl = thumbUrl;
 
-        return this;
+	return this;
     }
 
     public JsonObject toJson() {
-        JsonObject data = new JsonObject();
+	JsonObject data = new JsonObject();
 
-        if (fallback == null) {
-            throw new IllegalArgumentException(
-                    "Missing Fallback @ SlackAttachment");
-        } else {
-            data.addProperty(FALLBACK, fallback);
-        }
+	if (fallback == null) {
+	    throw new IllegalArgumentException("Missing Fallback @ SlackAttachment");
+	} else {
+	    data.addProperty(FALLBACK, fallback);
+	}
 
-        if (text != null) {
-            data.addProperty(TEXT, text);
-        }
+	if (text != null) {
+	    data.addProperty(TEXT, text);
+	}
 
-        if (pretext != null) {
-            data.addProperty(PRETEXT, pretext);
-        }
+	if (pretext != null) {
+	    data.addProperty(PRETEXT, pretext);
+	}
 
-        if (color != null) {
-            data.addProperty(COLOR, color);
-        }
+	if (color != null) {
+	    data.addProperty(COLOR, color);
+	}
 
-        if (authorName != null) {
-            data.addProperty(AUTHOR_NAME, authorName);
-        }
+	if (authorName != null) {
+	    data.addProperty(AUTHOR_NAME, authorName);
+	}
 
-        if (authorLink != null) {
-            data.addProperty(AUTHOR_LINK, authorLink);
-        }
+	if (authorLink != null) {
+	    data.addProperty(AUTHOR_LINK, authorLink);
+	}
 
-        if (authorIcon != null) {
-            data.addProperty(AUTHOR_ICON, authorIcon);
-        }
+	if (authorIcon != null) {
+	    data.addProperty(AUTHOR_ICON, authorIcon);
+	}
 
-        if (title != null) {
-            data.addProperty(TITLE, title);
-        }
+	if (title != null) {
+	    data.addProperty(TITLE, title);
+	}
 
-        if (titleLink != null) {
-            data.addProperty(TITLE_LINK, titleLink);
-        }
+	if (titleLink != null) {
+	    data.addProperty(TITLE_LINK, titleLink);
+	}
 
-        if (imageUrl != null) {
-            data.addProperty(IMAGE_URL, imageUrl);
-        }
+	if (imageUrl != null) {
+	    data.addProperty(IMAGE_URL, imageUrl);
+	}
 
-        if (thumbUrl != null) {
-            data.addProperty(THUMB_URL, thumbUrl);
-        }
+	if (thumbUrl != null) {
+	    data.addProperty(THUMB_URL, thumbUrl);
+	}
 
-        if (fields != null && fields.size() > 0) {
-            data.add(FIELDS, prepareFields());
-        }
+	if (markdownAttributes != null) {
+	    data.add(MRKDWN_IN, prepareMarkdownAttributes());
+	}
 
-        return data;
+	if (fields != null && fields.size() > 0) {
+	    data.add(FIELDS, prepareFields());
+	}
+
+	return data;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	if (this == o)
+	    return true;
+	if (o == null || getClass() != o.getClass())
+	    return false;
 
-        SlackAttachment that = (SlackAttachment) o;
+	SlackAttachment that = (SlackAttachment) o;
 
-        if (fallback != null ? !fallback.equals(that.fallback) : that.fallback != null) return false;
-        if (text != null ? !text.equals(that.text) : that.text != null) return false;
-        if (pretext != null ? !pretext.equals(that.pretext) : that.pretext != null) return false;
-        if (color != null ? !color.equals(that.color) : that.color != null) return false;
-        if (authorName != null ? !authorName.equals(that.authorName) : that.authorName != null) return false;
-        if (authorLink != null ? !authorLink.equals(that.authorLink) : that.authorLink != null) return false;
-        if (authorIcon != null ? !authorIcon.equals(that.authorIcon) : that.authorIcon != null) return false;
-        if (title != null ? !title.equals(that.title) : that.title != null) return false;
-        if (titleLink != null ? !titleLink.equals(that.titleLink) : that.titleLink != null) return false;
-        if (imageUrl != null ? !imageUrl.equals(that.imageUrl) : that.imageUrl != null) return false;
-        if (thumbUrl != null ? !thumbUrl.equals(that.thumbUrl) : that.thumbUrl != null) return false;
-        return !(fields != null ? !fields.equals(that.fields) : that.fields != null);
+	if (fallback != null ? !fallback.equals(that.fallback) : that.fallback != null)
+	    return false;
+	if (text != null ? !text.equals(that.text) : that.text != null)
+	    return false;
+	if (pretext != null ? !pretext.equals(that.pretext) : that.pretext != null)
+	    return false;
+	if (color != null ? !color.equals(that.color) : that.color != null)
+	    return false;
+	if (authorName != null ? !authorName.equals(that.authorName) : that.authorName != null)
+	    return false;
+	if (authorLink != null ? !authorLink.equals(that.authorLink) : that.authorLink != null)
+	    return false;
+	if (authorIcon != null ? !authorIcon.equals(that.authorIcon) : that.authorIcon != null)
+	    return false;
+	if (title != null ? !title.equals(that.title) : that.title != null)
+	    return false;
+	if (titleLink != null ? !titleLink.equals(that.titleLink) : that.titleLink != null)
+	    return false;
+	if (imageUrl != null ? !imageUrl.equals(that.imageUrl) : that.imageUrl != null)
+	    return false;
+	if (thumbUrl != null ? !thumbUrl.equals(that.thumbUrl) : that.thumbUrl != null)
+	    return false;
+	if (markdownAttributes != null ? !markdownAttributes.equals(that.markdownAttributes)
+		: that.markdownAttributes != null)
+	    return false;
+	return !(fields != null ? !fields.equals(that.fields) : that.fields != null);
 
     }
 
     @Override
     public int hashCode() {
-        int result = fallback != null ? fallback.hashCode() : 0;
-        result = 31 * result + (text != null ? text.hashCode() : 0);
-        result = 31 * result + (pretext != null ? pretext.hashCode() : 0);
-        result = 31 * result + (color != null ? color.hashCode() : 0);
-        result = 31 * result + (authorName != null ? authorName.hashCode() : 0);
-        result = 31 * result + (authorLink != null ? authorLink.hashCode() : 0);
-        result = 31 * result + (authorIcon != null ? authorIcon.hashCode() : 0);
-        result = 31 * result + (title != null ? title.hashCode() : 0);
-        result = 31 * result + (titleLink != null ? titleLink.hashCode() : 0);
-        result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
-        result = 31 * result + (thumbUrl != null ? thumbUrl.hashCode() : 0);
-        result = 31 * result + (fields != null ? fields.hashCode() : 0);
-        return result;
+	int result = fallback != null ? fallback.hashCode() : 0;
+	result = 31 * result + (text != null ? text.hashCode() : 0);
+	result = 31 * result + (pretext != null ? pretext.hashCode() : 0);
+	result = 31 * result + (color != null ? color.hashCode() : 0);
+	result = 31 * result + (authorName != null ? authorName.hashCode() : 0);
+	result = 31 * result + (authorLink != null ? authorLink.hashCode() : 0);
+	result = 31 * result + (authorIcon != null ? authorIcon.hashCode() : 0);
+	result = 31 * result + (title != null ? title.hashCode() : 0);
+	result = 31 * result + (titleLink != null ? titleLink.hashCode() : 0);
+	result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
+	result = 31 * result + (thumbUrl != null ? thumbUrl.hashCode() : 0);
+	result = 31 * result + (markdownAttributes != null ? markdownAttributes.hashCode() : 0);
+	result = 31 * result + (fields != null ? fields.hashCode() : 0);
+	return result;
     }
 
     @Override
     public String toString() {
-        return "SlackAttachment{" +
-                "fallback='" + fallback + '\'' +
-                ", text='" + text + '\'' +
-                ", pretext='" + pretext + '\'' +
-                ", color='" + color + '\'' +
-                ", authorName='" + authorName + '\'' +
-                ", authorLink='" + authorLink + '\'' +
-                ", authorIcon='" + authorIcon + '\'' +
-                ", title='" + title + '\'' +
-                ", titleLink='" + titleLink + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", thumbUrl='" + thumbUrl + '\'' +
-                ", fields=" + fields +
-                '}';
+	return "SlackAttachment{" + "fallback='" + fallback + '\'' + ", text='" + text + '\'' + ", pretext='" + pretext
+		+ '\'' + ", color='" + color + '\'' + ", authorName='" + authorName + '\'' + ", authorLink='"
+		+ authorLink + '\'' + ", authorIcon='" + authorIcon + '\'' + ", title='" + title + '\''
+		+ ", titleLink='" + titleLink + '\'' + ", imageUrl='" + imageUrl + '\'' + ", thumbUrl='" + thumbUrl
+		+ '\'' + ", markdownAttributes=" + markdownAttributes + ", fields=" + fields + '}';
     }
 }
